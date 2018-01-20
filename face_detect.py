717ed42dceda648fc8d50b814213e33a7c9e8011
vandanparmar <vandan@hotmail.co.uk>
2018-01-20 14:42:44 +0000

    added video reading

--- face_detect.py
+++ face_detect.py
@@ -7,29 +7,35 @@ import cv2
 
 # code from https://www.pyimagesearch.com/2017/04/03/facial-landmarks-dlib-opencv-python/
 
-def rect_to_bb(rect):
-    # take a bounding predicted by dlib and convert it
-    # to the format (x, y, w, h) as we would normally do
-    # with OpenCV
-    x = rect.left()
-    y = rect.top()
-    w = rect.right() - x
-    h = rect.bottom() - y
+filename = 'cameron.mp4'
 
-    # return a tuple of (x, y, w, h)
-    return (x, y, w, h)
+
+cap = cv2.VideoCapture(filename) 
+
+
+def rect_to_bb(rect):
+	# take a bounding predicted by dlib and convert it
+	# to the format (x, y, w, h) as we would normally do
+	# with OpenCV
+	x = rect.left()
+	y = rect.top()
+	w = rect.right() - x
+	h = rect.bottom() - y
+
+	# return a tuple of (x, y, w, h)
+	return (x, y, w, h)
 
 def shape_to_np(shape, dtype="int"):
-    # initialize the list of (x, y)-coordinates
-    coords = np.zeros((68, 2), dtype=dtype)
+	# initialize the list of (x, y)-coordinates
+	coords = np.zeros((68, 2), dtype=dtype)
 
-    # loop over the 68 facial landmarks and convert them
-    # to a 2-tuple of (x, y)-coordinates
-    for i in range(0, 68):
-        coords[i] = (shape.part(i).x, shape.part(i).y)
+	# loop over the 68 facial landmarks and convert them
+	# to a 2-tuple of (x, y)-coordinates
+	for i in range(0, 68):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
 
-    # return the list of (x, y)-coordinates
-    return coords
+	# return the list of (x, y)-coordinates
+	return coords
 
 # initialize dlib's face detector (HOG-based) and then create
 # the facial landmark predictor
@@ -37,31 +43,35 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 # load the input image, resize it, and convert it to grayscale
-image = cv2.imread("image.png")
-image = imutils.resize(image, width=500)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ret = True
+while(ret == True):
+	ret, image = cap.read()
+	#image = cv2.imread(frame)
+	image = imutils.resize(image, width=500)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# detect faces in the grayscale image
-rects = detector(gray, 1)
+	# detect faces in the grayscale image
+	rects = detector(gray, 1)
 
-for (i, rect) in enumerate(rects):
-    # determine the facial landmarks for the face region, then
-    # convert the facial landmark (x, y)-coordinates to a NumPy
-    # array
-    shape = predictor(gray, rect)
-    shape = face_utils.shape_to_np(shape)
+	for (i, rect) in enumerate(rects):
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
 
-    # convert dlib's rectangle to a OpenCV-style bounding box
-    # [i.e., (x, y, w, h)], then draw the face bounding box
-    (x, y, w, h) = face_utils.rect_to_bb(rect)
-    print(x, y, w, h)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		# convert dlib's rectangle to a OpenCV-style bounding box
+		# [i.e., (x, y, w, h)], then draw the face bounding box
+		(x, y, w, h) = face_utils.rect_to_bb(rect)
+#        print(x, y, w, h)
+		cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # loop over the (x, y)-coordinates for the facial landmarks
-    # and draw them on the image
-    for index, (x, y) in enumerate(shape):
-        cv2.circle(image, (x, y), 1, ((255/80)*index, (255/80)*index, (255/80)*index), -1)
+		# loop over the (x, y)-coordinates for the facial landmarks
+		# and draw them on the image
+		for index, (x, y) in enumerate(shape):
+			cv2.circle(image, (x, y), 1, ((255/80)*index, (255/80)*index, (255/80)*index), -1)
 
-# show the output image with the face detections + facial landmarks
-cv2.imshow("Output", image)
-cv2.waitKey(0)
+	# show the output image with the face detections + facial landmarks
+	cv2.imshow("Output", image)
+
+
